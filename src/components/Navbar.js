0b82205f09df9102d9416a7017ca323d34a47f54
Safet Salkanović <salkanovicsafet@gmail.com>
2022-02-03 14:49:02 +0100

    Write Navbar.js css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import logo from "../assets/images/logo.png";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="container">
+      <div className="navbar__flex container">
         <img src={logo} alt="Logo" className="navbar__logo" />
         <div className="navbar__title">my travel journal.</div>
       </div>
