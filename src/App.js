45873488f2719e07a1b75c0d074597b36112dd4d
Safet Salkanović <salkanovicsafet@gmail.com>
2022-02-03 16:20:58 +0100

    Make Destination.js reusable with data from data.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Destination from "./components/Destination";
 import Navbar from "./components/Navbar";
+import destinationsData from "./data.js";
+
+const destinations = destinationsData.map((x) => (
+  <Destination key={x.title} item={x} />
+));
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <div className="container main">
-        <Destination />
-        <Destination />
-      </div>
+      <div className="container main">{destinations}</div>
     </div>
   );
 }
--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,13 +1,10 @@
 import PathIcon from "../assets/images/Path.png";
 
-export default function Destination() {
+export default function Destination(props) {
   return (
     <div className="destination flex">
       <div className="destination__photo">
-        <img
-          src="https://images.unsplash.com/photo-1589308078059-be1415eab4c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt="Mount Fuji"
-        />
+        <img src={props.item.imageUrl} alt={props.item.title} />
       </div>
       <div className="destination__info">
         <div className="destination__info__location flex">
@@ -16,22 +13,22 @@ export default function Destination() {
             alt="Path Icon"
             className="destination__info__location__pathIcon"
           />
-          <div className="destination__info__location__country">Japan</div>
+          <div className="destination__info__location__country">
+            {props.item.location}
+          </div>
           <a
-            href="https://goo.gl/maps/1DGM5WrWnATgkSNB8"
+            href={props.item.googleMapsUrl}
             className="destination__info__location__gmaps"
           >
             View on Google Maps
           </a>
         </div>
-        <h1 className="destination__info__name">Mount Fuji</h1>
+        <h1 className="destination__info__name">{props.item.title}</h1>
         <div className="destination__info__date">
-          12 Jan, 2021 - 24 Jan, 2021
+          {props.item.startDate} - {props.item.endDate}
         </div>
         <div className="destination__info__content">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
+          {props.item.description}
         </div>
       </div>
     </div>
